40a286e0cb02780d693df7d629733537b49a10fd
Brittany Halterman <halterma@gmail.com>
2025-06-02 19:10:44 -0400

    Refactor rank it block spec

--- spec/card_deck_spec.rb
+++ spec/card_deck_spec.rb
@@ -23,19 +23,9 @@ describe 'CardDeck' do
   it 'should include every rank' do
     deck = CardDeck.new
 
-    expect(deck.cards.map(&:rank)).to include('2')
-    expect(deck.cards.map(&:rank)).to include('3')
-    expect(deck.cards.map(&:rank)).to include('4')
-    expect(deck.cards.map(&:rank)).to include('5')
-    expect(deck.cards.map(&:rank)).to include('6')
-    expect(deck.cards.map(&:rank)).to include('7')
-    expect(deck.cards.map(&:rank)).to include('8')
-    expect(deck.cards.map(&:rank)).to include('9')
-    expect(deck.cards.map(&:rank)).to include('10')
-    expect(deck.cards.map(&:rank)).to include('J')
-    expect(deck.cards.map(&:rank)).to include('Q')
-    expect(deck.cards.map(&:rank)).to include('K')
-    expect(deck.cards.map(&:rank)).to include('A')
+    PlayingCard::RANK.each do |rank|
+      expect(deck.cards.map(&:rank)).to include(rank)
+    end
   end
 
   it 'Should have 52 cards when created' do
