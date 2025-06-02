942702534fb984a83a97f1be8e2ee7081d97274b
Brittany Halterman <halterma@gmail.com>
2025-06-02 16:36:32 -0400

    Include test to verify that a card deck includes every rank

--- spec/card_deck_spec.rb
+++ spec/card_deck_spec.rb
@@ -14,14 +14,29 @@ describe 'CardDeck' do
   it 'should include every suit' do
     deck = CardDeck.new
 
-    puts deck.cards.map(&:suit).inspect
     expect(deck.cards.map(&:suit)).to include('C')
     expect(deck.cards.map(&:suit)).to include('D')
     expect(deck.cards.map(&:suit)).to include('H')
     expect(deck.cards.map(&:suit)).to include('S')
   end
 
-  it 'should include every rank'
+  it 'should include every rank' do
+    deck = CardDeck.new
+
+    expect(deck.cards.map(&:rank)).to include('2')
+    expect(deck.cards.map(&:rank)).to include('3')
+    expect(deck.cards.map(&:rank)).to include('4')
+    expect(deck.cards.map(&:rank)).to include('5')
+    expect(deck.cards.map(&:rank)).to include('6')
+    expect(deck.cards.map(&:rank)).to include('7')
+    expect(deck.cards.map(&:rank)).to include('8')
+    expect(deck.cards.map(&:rank)).to include('9')
+    expect(deck.cards.map(&:rank)).to include('10')
+    expect(deck.cards.map(&:rank)).to include('J')
+    expect(deck.cards.map(&:rank)).to include('Q')
+    expect(deck.cards.map(&:rank)).to include('K')
+    expect(deck.cards.map(&:rank)).to include('A')
+  end
 
   it 'Should have 52 cards when created' do
     deck = CardDeck.new
