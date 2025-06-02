3eabc7c135ebea08d5c7c3d8e8497c4273f1a123
Brittany Halterman <halterma@gmail.com>
2025-06-02 11:27:13 -0400

    Initialize a card with rank and suit

--- spec/playing_card_spec.rb
+++ spec/playing_card_spec.rb
@@ -1,4 +1,10 @@
 require_relative '../lib/playing_card'
 
 describe 'PlayingCard' do
+  it 'should initialize with a rank and suit' do
+    card = PlayingCard.new('A', 'H')
+
+    expect(card.rank).to eq('A')
+    expect(card.suit).to eq('H')
+  end
 end
