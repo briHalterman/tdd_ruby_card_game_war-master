a7cd4f0fe88ec3f5af12705c187ec90c65ea5d9f
Brittany Halterman <halterma@gmail.com>
2025-06-02 14:23:30 -0400

    Initialize card deck with 52 cards

--- lib/card_deck.rb
+++ lib/card_deck.rb
@@ -1,11 +1,21 @@
+require_relative 'playing_card'
+
 class CardDeck
-  def cards_left
+  attr_accessor :cards
+  attr_accessor :cards_left
+
+  def initialize
+    @cards = []
     52.times do
-      PlayingCard.new('A', 'H')
+      @cards.push(PlayingCard.new('A', 'H'))
     end
   end
 
+  def cards_left
+    @cards
+  end
+
   def deal
-    @cards_left = cards_left - 1
+    @cards.pop
   end
 end
