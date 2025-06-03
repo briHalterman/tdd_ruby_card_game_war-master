ecee6b4845e5182f8fc97188634b672d9131781e
Brittany Halterman <halterma@gmail.com>
2025-06-02 21:03:43 -0400

    Initialize game with a deck

--- lib/war_game.rb
+++ lib/war_game.rb
@@ -1,9 +1,10 @@
 class WarGame
-  attr_accessor :player1, :player2
+  attr_accessor :player1, :player2, :deck
 
   def initialize
     @player1 = WarPlayer.new('Player1')
     @player2 = WarPlayer.new('Player2')
+    @deck = CardDeck.new
   end
 
 end
--- spec/war_game_spec.rb
+++ spec/war_game_spec.rb
@@ -1,6 +1,12 @@
 require_relative '../lib/war_game'
 
 describe 'WarGame' do
+  it 'initializes a game with a deck' do
+    game = WarGame.new
+
+    expect(game.deck).to respond_to :cards
+  end
+
   it 'initializes a game with two players' do
     game = WarGame.new
 
