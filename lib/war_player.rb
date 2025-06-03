8411a52f7fa43067d674fcec230c32d4ff94d8e6
Brittany Halterman <halterma@gmail.com>
2025-06-02 20:03:45 -0400

    Initialize war player with name

--- lib/war_player.rb
+++ lib/war_player.rb
@@ -1,1 +1,7 @@
- 
+class WarPlayer
+  attr_accessor :player_name
+
+  def initialize(player_name)
+    @player_name = player_name
+  end
+end
--- spec/war_player_spec.rb
+++ spec/war_player_spec.rb
@@ -1,4 +1,10 @@
 require_relative '../lib/war_player'
 
 describe 'WarPlayer' do
+  it 'initializes with a name' do
+    player = WarPlayer.new('PlayerX')
+
+    puts @player
+    expect(player.player_name).to eq 'PlayerX'
+  end
 end
