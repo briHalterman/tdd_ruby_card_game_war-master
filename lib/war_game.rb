18de53962d4180bd2b6c2a8d784a1ed2aff8b55b
Brittany Halterman <halterma@gmail.com>
2025-06-02 21:01:18 -0400

    Initialize game with two players

--- lib/war_game.rb
+++ lib/war_game.rb
@@ -1,1 +1,9 @@
-# initialize two war players and a war object
+class WarGame
+  attr_accessor :player1, :player2
+
+  def initialize
+    @player1 = WarPlayer.new('Player1')
+    @player2 = WarPlayer.new('Player2')
+  end
+
+end
--- spec/war_game_spec.rb
+++ spec/war_game_spec.rb
@@ -1,4 +1,10 @@
 require_relative '../lib/war_game'
 
 describe 'WarGame' do
+  it 'initializes a game with two players' do
+    game = WarGame.new
+
+    expect(game.player1).to be_a WarPlayer
+    expect(game.player2).to be_a WarPlayer
+  end
 end
