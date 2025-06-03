74b91656bfbf7a15ecc7a16c8cd40cf48c76634a
Brittany Halterman <halterma@gmail.com>
2025-06-02 21:18:26 -0400

    Refactor war game spec with lazy let

--- spec/war_game_spec.rb
+++ spec/war_game_spec.rb
@@ -1,24 +1,20 @@
 require_relative '../lib/war_game'
 
 describe 'WarGame' do
-  it 'initializes a game with a deck' do
-    game = WarGame.new
+  let(:game) { WarGame.new }
 
+  it 'initializes a game with a deck' do
     expect(game.deck).to respond_to :cards
   end
 
   it 'initializes a game with two players' do
-    game = WarGame.new
-
     expect(game.player1).to be_a WarPlayer
     expect(game.player2).to be_a WarPlayer
   end
 
   it 'deals a card to a player' do
-    game = WarGame.new
     game.deal_card(game.player1)
 
-    puts game.player1
     expect(game.player1.player_stack.count).to eq 1
   end
 end
