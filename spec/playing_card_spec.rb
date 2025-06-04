617a6706f041810fd1f6d3626a828031c1a74980
Brittany Halterman <halterma@gmail.com>
2025-06-04 14:26:55 -0400

    Remove card comparison tests from playing card spec

--- spec/playing_card_spec.rb
+++ spec/playing_card_spec.rb
@@ -1,34 +1,30 @@
 require_relative '../lib/playing_card'
 
 describe 'PlayingCard' do
-  it 'should initialize with a rank and suit' do
-    card = PlayingCard.new('A', 'H')
+  describe 'initialize' do
+    it 'should initialize with a rank and suit' do
+      card = PlayingCard.new('A', 'H')
 
-    expect(card.rank).to eq('A')
-    expect(card.suit).to eq('H')
-  end
+      expect(card.rank).to eq('A')
+      expect(card.suit).to eq('H')
+    end
 
-  it 'should be a valid suit' do
-    expect{
-      PlayingCard.new('A', 'X')
-    }.to raise_error StandardError
-  end
+    it 'should be a valid suit' do
+      expect{
+        PlayingCard.new('A', 'X')
+      }.to raise_error StandardError
+    end
 
-  it 'should be a valid rank' do
-    expect{
-      PlayingCard.new('X', 'H')
-  }.to raise_error StandardError
-  end
+    it 'should be a valid rank' do
+      expect{
+        PlayingCard.new('X', 'H')
+      }.to raise_error StandardError
+    end
 
-  it 'should assign the correct value to a card\'s suit' do
-    card = PlayingCard.new('A', 'H')
+    it 'should assign the correct value to a card\'s suit' do
+      card = PlayingCard.new('A', 'H')
 
-    expect(card.value).to eq 12
-  end
-
-  it 'should correctly compare two cards of the same rank and suit' do
-    card1 = PlayingCard.new('A', 'H')
-    card2 = PlayingCard.new('A', 'H')
-    expect(card1).to eq card2
+      expect(card.value).to eq 12
+    end
   end
 end
